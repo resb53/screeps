c725149ab2d4fe01cc3cdf4d2d049305fdbe345c
resb53 <resb53.git@gmail.com>
2018-06-12 16:59:19 +0100

    Ensure we always have as many harvesters as wanted

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 var roleHarvester = require('role.harvester');
 
-module.exports.loop = function () {
+HARVESTERS_WANTED = 1;
 
+module.exports.loop = function () {
+    //Garbage collection
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory: ', name);
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    if (harvesters.length < HARVESTERS_WANTED) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE,MOVE], newName, {memory: {role: 'harvester'}});
+    }
+
+    if (Game.spawns['Spawn1'].spawning) {
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️ ' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1,
+            Game.spawns['Spawn1'].pos.y,
+            {align: 'left', opacity: 0.8}
+        );
+    }
 }
